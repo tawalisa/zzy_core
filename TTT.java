b6c96c486b980896096c6650fe656531a306a4f0
tawalisa <tawalisa@163.com>
2022-06-16 16:08:52 +0800

    Create TTT.java

---
+++ TTT.java
@@ -0,0 +1,7 @@
+public class TTT {
+    public static void main(String[] args) throws InterruptedException {
+        byte[][][][] a = new byte[4][1024][1024][1024];
+        Thread.sleep(1000L * 60 * 60 * 60);
+        System.out.println(a);
+    }
+}
